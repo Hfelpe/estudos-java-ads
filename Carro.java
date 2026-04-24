eeb935bb8bb84530fbb9e2ba567dce3cb17b1dd8
Felipe Natan-dev <felipens862@gmail.com>
2026-04-24 16:25:43 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,10 @@
+public class Carro {
+    // Atributos (O que o carro TEM)
+    String modelo;
+    String placa;
+
+    // Método (O que o carro FAZ)
+    public void ligar() {
+        System.out.println("O carro " + modelo + " está ligado!");
+    }
+}
