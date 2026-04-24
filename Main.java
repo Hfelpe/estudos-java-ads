6d9308be975f27456f71362a8263746d108c5e5a
Hfelpe <felipens862@gmail.com>
2026-04-24 15:15:57 -0300

    feat: meu primeiro código java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando minha jornada como desenvolvedor Java!");
+        System.out.println("Foco: Estágio em ADS.");
+        
+        int horasDeEstudoHoje = 8; 
+        System.out.println("Meta de estudos para hoje: " + horasDeEstudoHoje + " horas.");
+    }
