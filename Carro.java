6a88dd1740929f79c9d8a5c74d455b99d70bbcd4
Felipe Natan-dev <felipens862@gmail.com>
2026-04-24 16:26:21 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,9 +1,7 @@
 public class Carro {
-    // Atributos (O que o carro TEM)
     String modelo;
     String placa;
 
-    // Método (O que o carro FAZ)
     public void ligar() {
         System.out.println("O carro " + modelo + " está ligado!");
     }
